4ed42a5f73485e1d223c6a995aa90fcffd961539
tchiks1 <tchikscorporation@gmail.com>
2022-04-29 12:34:57 +0100

    29avril

---
+++ vendor/imanghafoori/php-abstract-filesystem/src/RealFilesystem.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Imanghafoori\Filesystem;
+
+class RealFilesystem
+{
+    public static function __callStatic($method, $params)
+    {
+        $methods = [
+            'file_get_contents',
+            'file_put_contents',
+            'feof',
+            'fopen',
+            'fgets', 'fgetc',
+            'fwrite',
+            'rename',
+            'unlink',
+            'mkdir',
+            'rmdir',
+            'ftruncate',
+            'fflush',
+            'touch',
+            'fclose',
+        ];
+
+        if (in_array($method, $methods, true)) {
+            return $method(...$params);
+        }
+    }
+}
